25c1e22873585cb7eb7eb0b0d0e07172b301721a
Buddynice34 <sirapril34@gmail.com>
2023-09-04 11:49:27 +0200

    Added a connection string

--- TzFastFood/TzFastFood/Models/TzFastFoodDbContext.cs
+++ TzFastFood/TzFastFood/Models/TzFastFoodDbContext.cs
@@ -1,6 +1,42 @@
-﻿namespace TzFastFood.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TzFastFood.Models
 {
-    public class TzFastFoodDbContext
+    public class TzFastFoodDbContext : DbContext
     {
+        public TzFastFoodDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Define relationships here
+                modelBuilder.Entity<Customer>()
+                .HasMany(c => c.Orders)
+                .WithOne(o => o.Customer)
+                .HasForeignKey(o => o.CustomerId);
+
+                modelBuilder.Entity<Category>()
+                .HasMany(cat => cat.Products)
+                .WithOne(prod => prod.Category)
+                .HasForeignKey(prod => prod.CategoryId);
+
+                modelBuilder.Entity<Product>()
+                .HasMany(prod => prod.OrderItems)
+                .WithOne(oi => oi.Product)
+                .HasForeignKey(oi => oi.ProductId);
+
+                modelBuilder.Entity<Order>()
+                .HasMany(o => o.OrderItems)
+                .WithOne(oi => oi.Order)
+                .HasForeignKey(oi => oi.OrderId);
+        }
     }
 }
+
